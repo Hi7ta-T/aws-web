bb27def5cbde6229f1722d983237c23bed980d9e
Hinata <mistk_72203102@yahoo.co.jp>
2026-06-14 21:45:18 +0900

    nat create

--- terraform/nat.tf
+++ terraform/nat.tf
@@ -1,12 +1,28 @@
+resource "aws_eip" "eip-az1" {
+  domain = "vpc"
+  tags {
+    Name = "eip-az1"
+  }
+}
+
 resource "aws_nat_gateway" "nat-az1" {
   vpc_id = "aws_vpc.main.id"
+  allocation_id = "aws_eip.eip-az1.id"
   tags {
     Name = "aws-nat-az1"
   }
 }
 
+resource "aws_eip" "eip-az2" {
+  domain = "vpc"
+  tags {
+    Name = "eip-az2"
+  }
+}
+
 resource "aws_nat_gateway" "nat-az2" {
   vpc_id = "aws_vpc.main.id"
+  allocation_id = "aws_eip.eip-az2.id"
   tags {
     Name = "aws-nat-az2"
   }

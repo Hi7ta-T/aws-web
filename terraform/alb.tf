feb18802c35f0a2002135886e93870289f0b5f6e
Hinata <mistk_72203102@yahoo.co.jp>
2026-06-15 22:07:42 +0900

    fix

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,8 +1,8 @@
 resource "aws_lb" "aws-alb" {
   vpc_id             = aws_vpc.main.id
-  subnet_ids         = [aws_subnet.public-1a.id, aws_subnet.public-1c.id]
+  subnet_id          = [aws_subnet.public-1a.id, aws_subnet.public-1c.id]
   load_balancer_type = "application"
-  secutity_groups    = "aws_security_group.alb_sg.id"
+  secutity_groups    = aws_security_group.alb_sg.id
   tags {
     Name = "alb"
   }

e9e6f6bb6c7b8d6c9bb73581e35d31c4ef35af8e
Hinata <mistk_72203102@yahoo.co.jp>
2026-06-22 00:36:44 +0900

    fix trust_policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,4 +1,4 @@
-# 信頼ポリシー
+# Trust_policy
 resource "aws_iam_role" "ec2-iam" {
     name = "ec2_iam"
     assume_role_policy = jsonencode ({
@@ -6,15 +6,26 @@ resource "aws_iam_role" "ec2-iam" {
         Statement [{
             Effect = "Allow"
             Principal = {
-                Service =
+                Service = "ec2.amazonaws.com"
              }
                 Action = "sts:AssumeRole"
             }]
         })
     }
 
-# 権限ポリシー
+# Permission_policy
 resource "aws_iam_role_policy_attachment" "ec2-iam" 
+    assume_role_policy = jsonencode ({
+        Version = "2012-10-17"
+        Statement [{
+            Effect = "Allow"
+            Principal = {
+                Service = "ec2.amazonaws.com"
+             }
+                Action = ""
+            }]
+        })
+    }
 
 # instance_profile
 resource "aws_iam_instance_profile" "ec2-iam"

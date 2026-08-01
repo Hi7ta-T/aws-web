ad8f67d0acb502633edd520815162d12045ba79b
Hinata Takahashi <mistk_72203102@yahoo.co.jp>
2026-08-02 01:24:22 +0900

    Update user.sql

--- db/ddl/user.sql
+++ db/ddl/user.sql
@@ -3,8 +3,12 @@ CREATE TABLE user (
   user_id INT PRIMARY KEY NOT NULL,
   nickname VARCHAR(20) NOT NULL,
   password VARCHAR(60) NOT NULL,
-  current_pref ENUM NOT NULL,
   current_city VARCHAR(20),
-  age_group ENUM NOT NULL,
-  gender ENUM NOT NULL
+  age_group ENUM(
+   '10代', '20代', '30代', 
+   '40代', '50代', '60代', 
+   '70代~') NOT NULL
+  gender ENUM(
+   '男性', '女性','未回答') NOT NULL
+
 );

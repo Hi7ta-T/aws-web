775100e4a0756fdf4b9181a453b7a7c353bcc2d7
Hinata Takahashi <mistk_72203102@yahoo.co.jp>
2026-08-02 01:12:15 +0900

    Update user.sql

--- db/ddl/user.sql
+++ db/ddl/user.sql
@@ -1,1 +1,10 @@
 
+CREATE TABLE user (
+  user_id INT PRIMARY KEY NOT NULL,
+  nickname VARCHAR(20) NOT NULL,
+  password VARCHAR(60) NOT NULL,
+  current_pref ENUM NOT NULL,
+  current_city VARCHAR(20),
+  age_group ENUM NOT NULL,
+  gender ENUM NOT NULL
+);

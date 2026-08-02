630b6edc232cfdba7b7b07a52dce46dcd9e499b6
Hinata Takahashi <mistk_72203102@yahoo.co.jp>
2026-08-02 23:54:34 +0900

    Update spots.sql

--- db/ddl/spots.sql
+++ db/ddl/spots.sql
@@ -1,9 +1,9 @@
 
 CREATE TABLE spots (
   spot_id INT PRIMARY KEY NOT NULL,
-  city VARCHAR(20) NOT NULL,
+  spot_city VARCHAR(20) NOT NULL,
   station_name VARCHAR(20) NOT NULL,
 
-  FOREIGN KEY (spot_pref)
+  FOREIGN KEY (spot_pref_id)
   REFERENCES prefecture(pref_id)
 );

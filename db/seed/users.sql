79dcb2c1e275849b3f61ff241f37c4279e2d5598
Hinata Takahashi <mistk_72203102@yahoo.co.jp>
2026-08-03 22:51:12 +0900

    Update and rename user.sql to users.sql

---
+++ db/seed/users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (user_id, nickname, password, recent_pref_id, recent_city, age_group, gender)
+VALUES (1, 'Emma', 'a1kPn35gay', 13, '','20代', '女性'),
+       (2, 'Tom', 'Zy2P9br50s',2, '弘前市','40代', '男性'),
+　　　 　(3, 'John', 'su3Wq7n48my', 46, '','50代', '男性');
+

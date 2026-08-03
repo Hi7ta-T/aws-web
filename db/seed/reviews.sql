30968a8b554c123b068f6c95dfbbbece705a2687
Hinata Takahashi <mistk_72203102@yahoo.co.jp>
2026-08-04 08:32:40 +0900

    Update reviews.sql

--- db/seed/reviews.sql
+++ db/seed/reviews.sql
@@ -1,2 +1,2 @@
-INSERT INTO reviews ()
-VALUES 
+INSERT INTO reviews (review_id,user_id,spot_id,rating,daytime_safety,night_safety,convenience,comment)
+VALUES (1, 2, 1, 4.5, 4.0, 3.8, 4.2, '繁華街のため夜の治安は悪い');

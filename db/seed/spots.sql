868d1969019b8a06a3e3e6f729239dbbb06623f0
Hinata Takahashi <mistk_72203102@yahoo.co.jp>
2026-08-04 08:24:54 +0900

    Update spots.sql

--- db/seed/spots.sql
+++ db/seed/spots.sql
@@ -1,3 +1,5 @@
-INSERT INTO spots ()
-VALUES 
+INSERT INTO spots (spot_id,spot_pref_id,spot_city,station_name)
+VALUES (1, 14, '横浜市西区', '横浜駅'),
+       (2, 26, '京都市下京区', '京都駅'),
+       (3, 4, '仙台市青葉区', '仙台駅');
 

7aea01354eef1abae48a7f85699a2078edf265c5
Hinata Takahashi <mistk_72203102@yahoo.co.jp>
2026-08-09 22:18:37 +0900

    Update users.sql

--- db/seed/users.sql
+++ db/seed/users.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (user_id, nickname, password, recent_pref_id, recent_city, age_group, gender)
-VALUES (1, 'Emma', 'a1kPn35gay', 13, '港区','20代', '女性'),
-       (2, 'Tom', 'Zy2P9br50s',2, '弘前市','40代', '男性'),
-　　　 　(3, 'John', 'su3Wq7n48my', 46, '','50代', '男性');
+VALUES (1, 'Emma', 'ハッシュ化後に記載', 13, '港区','20代', '女性'),
+       (2, 'Tom', 'ハッシュ化後に記載',2, '弘前市','40代', '男性'),
+　　　 　(3, 'John', 'ハッシュ化後に記載', 46, '','50代', '男性');
 
